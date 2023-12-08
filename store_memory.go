5ab86bd6f333aad3936f912fc52b411168dcd4a7
mojocn <neochau@gmail.com>
2023-12-08 12:48:09 +0000

    fix verify bug

--- store_memory.go
+++ store_memory.go
@@ -66,6 +66,9 @@ func (s *memoryStore) Set(id string, value string) error {
 }
 
 func (s *memoryStore) Verify(id, answer string, clear bool) bool {
+	if id == "" || answer == "" {
+		return false
+	}
 	v := s.Get(id, clear)
 	return v != "" && v == answer
 }
--- store_memory_test.go
+++ store_memory_test.go
@@ -143,6 +143,10 @@ func Test_memoryStore_Verify(t *testing.T) {
 	if got {
 		t.Error("failed3")
 	}
+	got = DefaultMemStore.Verify("saaf", "", true)
+	if got {
+		t.Error("CVE-2023-45292 GO-2023-2386")
+	}
 }
 
 func Test_memoryStore_Get(t *testing.T) {
